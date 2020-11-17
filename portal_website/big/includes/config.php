b750c93bfec1489e4bbd0cc2feec98efb775e531
meeks21 <anthony.meeks3@gmail.com>
2020-11-17 00:24:58 -0800

    fixes error

--- portal_website/big/includes/config.php
+++ portal_website/big/includes/config.php
@@ -23,63 +23,63 @@ switch(THIS_PAGE){
     case 'index.php':
         $title = 'Kemar\'s Web Dev Resources';
         $logo = 'fa-code';
-        $logo_color = 'style="color: rgb(28, 9, 48)"';
+        $logo_color = 'style="color: #f4f6f9)"';
         $PageID = 'Client Questionaire';
     break;
 
     case 'flexbox.php':
         $title = 'Introduction to Flexbox';
-        $logo = 'fa-code';
-        $logo_color = 'style="color: rgb(28, 9, 48)"';
+        $logo = 'fa-fw fa-cube';
+        $logo_color = 'style="color: #f4f6f9)"';
         $PageID = 'Flexbox';
     break;
 
     case 'galleries.php':
         $title = 'Galleries';
-        $logo = 'fa-envelope';
-        $logo_color = 'style="color: rgb(60, 21, 104)"';
+        $logo = 'fa-fw fa-camera-retro';
+        $logo_color = 'style="color: #f4f6f9"';
         $PageID = 'Galleries and Sliders';
     break;
 
     case 'shoppingcarts.php':
         $title = 'Shopping Carts';
-        $logo = 'fa-envelope';
-        $logo_color = 'style="color: rgb(60, 21, 104)"';
+        $logo = 'fa-fw fa-shopping-basket';
+        $logo_color = 'style="color: #f4f6f9"';
         $PageID = 'Shopping Carts';
     break;
 
     case 'siteapp.php':
         $title = 'Responsive vs. Mobile';
-        $logo = 'fa-envelope';
-        $logo_color = 'style="color: rgb(60, 21, 104)"';
+        $logo = 'fa-fw fa-html5';
+        $logo_color = 'style="color: #f4f6f9"';
         $PageID = 'Responsive Websites and Mobile Apps';
     break;
 
     case 'webcam.php':
         $title = 'Live Web Cam';
-        $logo = 'fa-envelope';
-        $logo_color = 'style="color: rgb(60, 21, 104)"';
+        $logo = 'fa-fw fa-eye';
+        $logo_color = 'style="color: #f4f6f9"';
         $PageID = 'NASA Earth From Space - Earth Viewing cameras ISS feed';
     break;
 
     case 'map.php':
         $title = 'Google Maps';
-        $logo = 'fa-envelope';
-        $logo_color = 'style="color: rgb(60, 21, 104)"';
+        $logo = 'fa-fw fa-map-o';
+        $logo_color = 'style="color: #f4f6f9"';
         $PageID = 'Embed Responsive Map';
     break;
 
     case 'calendar.php':
         $title = 'Responsive Calendar';
-        $logo = 'fa-envelope';
-        $logo_color = 'style="color: rgb(60, 21, 104)"';
+        $logo = 'fa-fw fa-calendar';
+        $logo_color = 'style="color: #f4f6f9"';
         $PageID = 'Calendar';
     break;
 
     case 'youtube.php':
-        $title = 'Website Accessibility';
-        $logo = 'fa-envelope';
-        $logo_color = 'style="color: rgb(60, 21, 104)"';
+        $title = 'Responsive YouTube';
+        $logo = 'fa-fw fa-youtube-square';
+        $logo_color = 'style="color: #f4f6f9"';
         $PageID = 'Website Accessibility and why it\'s important';
     break;
 
